130b801066c77f4767bea6e8433346c7dfa6080c
Alan Riquelme <alanriack@gmail.com>
2025-08-01 00:06:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os 
 from langchain_community.utilities import SerpAPIWrapper
-os.environ['SERPAPI_API_KEY'] = 'd1bf6d7574345617f5efc875360ab489a9ac2dbf3154c9b2ba13f3ecd9646bef'
+os.environ['SERPAPI_API_KEY'] = 'CLAVEAPIHERE'
 
 
 #creamos la instancia del buscador web
